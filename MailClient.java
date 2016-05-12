b8025d20956fb8b34885b99acd874b39e8a7d2d5
Duy Nguyen <nduytg@gmail.com>
2016-05-12 22:13:28 +0700

    Add files via upload

--- MailClient.java
+++ MailClient.java
@@ -1,5 +1,6 @@
 import java.security.Key;
 import java.util.Properties;
+import java.util.Vector;
 
 import javax.mail.*;
 
@@ -9,34 +10,102 @@ import javax.mail.internet.*;
 
 public class MailClient {
 
-	public static void main(String[] args)
+	private static class Person
 	{
-		//MailClient.abc();
-		MyCrypto cryptoHandler = new MyCrypto();
-		//cryptoHandler.digestMessage("HelloWorld", "SHA-1");
-		String mode = "AES";
-		int keySize = 256;
-		String key = cryptoHandler.createSymmetricKey(keySize, mode);
-		cryptoHandler.importSymmetricKey(key, mode);
-		cryptoHandler.encryptMessage(MyCrypto.stringToKey(key), "Hi, I'm Duy ^^");
+		public String username;
+		public String password;
+		
+		Person()
+		{
+			username = new String("");
+			password = new String("");
+		}
+		
+		Person(String user, String pass)
+		{
+			username = new String(user);
+			password = new String(pass);
+		}
+		
+		Person(String user)
+		{
+			username = new String(user);
+			password = new String("");
+		}
 	}
 	
-	public static void abc() {
-
-		final String username = "ipmansupern@gmail.com";
-		final String password = "12345678!!";
+	private static class MailContent
+	{
+		public Vector<String> recveivers;
+		public Vector<String> ccs;
+		public Vector<String> bccs;
+		public String host;
+		public String title;
+		public String content;
+		public Vector<String> fileList;
+		
+		
+		public String getHost() {
+			return host;
+		}
+		public void setHost(String host) {
+			this.host = host;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public String getContent() {
+			return content;
+		}
+		public void setContent(String content) {
+			this.content = content;
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		Person sender = new Person("ipmanxjackiechan@gmail.com","123a4567bcd");
+		Person receiver = new Person("zzzthanhcongzzz@gmail.com");
+		MailContent mailContent = new MailContent();
+		mailContent.setTitle("F*ck Cong!");
+		mailContent.setContent("F*ck you nln");
+		mailContent.setHost("smtp.gmail.com");
+		//mailContent.recveiver.
+		MailClient.sendMail(sender,receiver,mailContent);
+		//MyCrypto cryptoHandler = new MyCrypto();
+		//cryptoHandler.digestMessage("HelloWorld", "SHA-1");
+		//String mode = "AES";
+		//int keySize = 256;
+		//String key = cryptoHandler.createSymmetricKey(keySize, mode);
+		//cryptoHandler.importSymmetricKey(key, mode);
+		//cryptoHandler.encryptMessage(MyCrypto.stringToKey(key), "Hi, I'm Duy ^^");
+	}
+	
+	public static void sendMail(Person sender, Person receiver, MailContent mailContent) 
+	{
+		String username = sender.username;
+		String password = sender.password;
+		
 		System.out.println("Username: " + username);
 		System.out.println("Password: " + password);
 
+		// Sử dụng TLS
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
+		//props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.host", mailContent.getHost());
 		props.put("mail.smtp.port", "587");
 
+		// Tao session
 		Session session = Session.getInstance(props,
-		  new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
+		  new javax.mail.Authenticator() 
+		{
+			protected PasswordAuthentication getPasswordAuthentication() 
+			{
 				return new PasswordAuthentication(username, password);
 			}
 		  });
@@ -46,12 +115,16 @@ public class MailClient {
 			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress(username));
 			message.setRecipients(Message.RecipientType.TO,
-				InternetAddress.parse("nduytg@gmail.com"));
+				InternetAddress.parse(receiver.username));
 			message.setSubject("Testing Subject");
-			message.setText("Dear Mail Crawler,"
-				+ "\n\n Hello to my email, please!");
+			message.setText(mailContent.getContent());
 
-			Transport.send(message);
+			Transport transport = session.getTransport("smtp");
+			transport.connect(mailContent.getHost(), username, password);
+			transport.send(message);
+			transport.close();
+			
+			//Transport.close();
 
 			System.out.println("Done");
 
